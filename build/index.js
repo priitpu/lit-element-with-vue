eccc4bac1d231556332514f45e3a9444a28f5b98
Priit Puru <purupriit@gmail.com>
2020-03-12 12:06:46 +0200

    Squashed 'lib/t-components/' changes from 68f8e4d..36c095d
    36c095d .
    
    git-subtree-dir: lib/t-components
    git-subtree-split: 36c095dc29e91b367a07b5fa049803142503d04f

--- build/index.js
+++ build/index.js
@@ -3057,7 +3057,7 @@ let TButton = class TButton extends LitElement {
     return html`
       <button class="${classMap(this.classes)}">
         <span class="l-icon"><slot name="l-icon"></slot></span>
-        <h1><slot></slot></h1>
+        <span><slot></slot></span>
         <span class="r-icon"><slot name="r-icon"></slot></span>
       </button>
     `;
